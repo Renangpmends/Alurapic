dd27c5419928ffdf9f1f8d4fcac3897457b51b80
Renangpmends <renan.pereira.mendes@gmail.com>
2025-06-18 11:45:55 -0300

    adicionado a funcao filtro com o conhecimento intermediario em javascript ultilizando o vue computed para filtrar as imagens da api

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -3,8 +3,11 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
 
+
+    <input type="search" class="filtro" v-on:input="filtro = $event.target.value" placeholder="filtre por parte do titulo">
+    {{ filtro }}
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
             <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
@@ -30,7 +33,19 @@ export default {
     return {
 
       titulo: 'Alurapic', 
-      fotos: []
+      fotos: [],
+      filtro:''
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i')
+        return this.fotos.filter(foto => exp.test(foto.titulo))
+      }else {
+        return this.fotos
+      }
     }
   },
 
@@ -68,5 +83,10 @@ export default {
   .imagem-responsiva {
 
     width: 100%;
+  }
+
+  .filtro{
+    display: block;
+    width: 100%; 
   }
 </style>
